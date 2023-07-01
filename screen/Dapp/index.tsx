9705194ea81ecee223dd71d15ab0f746272bd3b6
caoxiaobin <cxiaobinwy@163.com>
2023-07-01 10:54:20 +0800

    [feature]: add dapp screen

---
+++ screen/Dapp/index.tsx
@@ -0,0 +1,8 @@
+import { makeStyles } from '@rneui/base';
+import Asset from '@screen/Asset';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+export const DAppScreen = () => {
+
+}
