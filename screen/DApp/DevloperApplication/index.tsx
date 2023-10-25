1db5074265673898edac96a4378cfec0ada29840
Shivanshu Pathak <97324405+shivanshu814@users.noreply.github.com>
2023-10-25 17:14:57 +0530

    feat: 入驻申请 screen

--- screen/DApp/DevloperApplication/index.tsx
+++ screen/DApp/DevloperApplication/index.tsx
@@ -1,67 +1,120 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { ScrollView, Text, TextInput, StyleSheet, Appearance, View } from 'react-native';
+import { Button } from 'react-native-ui-lib';
 
-const DevloperApplication = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const DeveloperApplication = () => {
+  const colorScheme = Appearance.getColorScheme();
+  const isDarkMode = colorScheme === 'dark';
 
-  const handleSignUp = () => {
-    // handle sign up logic here
-  };
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: isDarkMode ? '#1c1c1c' : '#fff',
+      paddingHorizontal: 30,
+      paddingVertical: 10,
+      paddingBottom: 20,
+    },
+    label: {
+      color: isDarkMode ? '#fff' : '#000',
+      fontSize: 10,
+      marginBottom: 10,
+    },
+    text: {
+      color: isDarkMode ? '#fff' : '#000',
+      fontSize: 14,
+      marginBottom: 10,
+    },
+    labels: {
+      color: isDarkMode ? '#808080' : '#000',
+      fontSize: 12,
+      marginBottom: 10,
+    },
+    input: {
+      backgroundColor: isDarkMode ? '#333' : '#f2f2f2',
+      color: isDarkMode ? '#fff' : '#000',
+      borderRadius: 5,
+      padding: 10,
+      marginBottom: 20,
+    },
+  });
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>开发者申请</Text>
-      <TextInput style={styles.input} placeholder="Name" value={name} onChangeText={setName} />
-      <TextInput style={styles.input} placeholder="Email" value={email} onChangeText={setEmail} />
+    <ScrollView style={styles.container}>
+      <Text style={styles.text}>开发者申请</Text>
+      <Text style={styles.labels}>需 审核， 需 KYC， 需签署 免责协议</Text>
+      <Text style={styles.label}>开发者联系邮箱</Text>
       <TextInput
         style={styles.input}
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
+        placeholder="ninsoidnk@gmail.com"
+        placeholderTextColor={isDarkMode ? '#999' : '#ccc'}
       />
-      <TouchableOpacity style={styles.button} onPress={handleSignUp}>
-        <Text style={styles.buttonText}>Sign Up</Text>
-      </TouchableOpacity>
-    </View>
+      <Text style={styles.label}>电话 (+082)</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="08214992848395"
+        placeholderTextColor={isDarkMode ? '#999' : '#ccc'}
+      />
+      <Text style={styles.label}>电报 (Telegram)</Text>
+      <TextInput style={styles.input} placeholder="windy0293" placeholderTextColor={isDarkMode ? '#999' : '#ccc'} />
+      <Text style={styles.label}>Twitter</Text>
+      <TextInput style={styles.input} placeholder="windy0293" placeholderTextColor={isDarkMode ? '#999' : '#ccc'} />
+      <Text style={styles.label}>Discord</Text>
+      <TextInput style={styles.input} placeholder="windy0293" placeholderTextColor={isDarkMode ? '#999' : '#ccc'} />
+
+      <Text style={styles.label}>项目/应用名称</Text>
+      <TextInput style={styles.input} placeholder="windy0293" placeholderTextColor={isDarkMode ? '#999' : '#ccc'} />
+
+      <Text style={styles.label}>项目/应用名称</Text>
+      <TextInput style={styles.input} placeholder="opensea" placeholderTextColor={isDarkMode ? '#999' : '#ccc'} />
+      <Text style={styles.label}>项目/应用官网</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="www.opeasea.com"
+        placeholderTextColor={isDarkMode ? '#999' : '#ccc'}
+      />
+
+      <Text style={styles.label}>应⽤团队创始⼈kyc，及对应⽩⽪书</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="NFT发行, NFT交易平台"
+        placeholderTextColor={isDarkMode ? '#999' : '#ccc'}
+      />
+
+      <Text style={styles.label}>项目/应用简介及所属类型</Text>
+      <TextInput style={styles.input} placeholder="SimonDirth" placeholderTextColor={isDarkMode ? '#999' : '#ccc'} />
+      <Text style={styles.label}>Token名称</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="www.opeasea.com"
+        placeholderTextColor={isDarkMode ? '#999' : '#ccc'}
+      />
+
+      <Text style={styles.label}>Token合约</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="www.opeasea.com"
+        placeholderTextColor={isDarkMode ? '#999' : '#ccc'}
+      />
+      <Text style={styles.label}>Token上所信息</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="www.opeasea.com"
+        placeholderTextColor={isDarkMode ? '#999' : '#ccc'}
+      />
+      <Text style={styles.label}>资本机构信息</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="www.opeasea.com"
+        placeholderTextColor={isDarkMode ? '#999' : '#ccc'}
+      />
+
+      <Text style={styles.labels}>*需签署免责声明及协议（平台免责，且有权</Text>
+
+      <View style={{ paddingBottom: 50 }}>
+        <Button label="提交申请" />
+      </View>
+    </ScrollView>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 32,
-    color: '#000',
-  },
-  input: {
-    width: '80%',
-    height: 48,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 4,
-    paddingHorizontal: 16,
-    marginBottom: 16,
-  },
-  button: {
-    backgroundColor: '#007AFF',
-    borderRadius: 4,
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-});
-
-export default DevloperApplication;
+export default DeveloperApplication;
