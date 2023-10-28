2b68eba04767dcd62536f85a5c3ff2f34eeefcae
Shivanshu Pathak <97324405+shivanshu814@users.noreply.github.com>
2023-10-29 04:03:31 +0530

    feat: review part

--- screen/DApp/ReportQuestion/index.tsx
+++ screen/DApp/ReportQuestion/index.tsx
@@ -1,69 +1,102 @@
-import React from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableOpacity, useColorScheme, Appearance } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import DocumentPicker from 'react-native-document-picker';
 
-const ReportQuestion = () => {
-  const colorScheme = Appearance.getColorScheme();
-  const isDarkMode = colorScheme === 'dark';
-
+const ReportQuestion = ({ isDarkMode }: { isDarkMode: boolean }) => {
+  const handlePress = async () => {
+    try {
+      const result = await DocumentPicker.pick({
+        type: [DocumentPicker.types.allFiles],
+      });
+      console.log(
+        'URI: ' + result.uri,
+        'Type: ' + result.type,
+        'File Name: ' + result.name,
+        'File Size: ' + result.size
+      );
+    } catch (err) {
+      if (DocumentPicker.isCancel(err)) {
+        Alert.alert('File selection was cancelled');
+      } else {
+        throw err;
+      }
+    }
+  };
+  const [reviewText, setReviewText] = useState('');
   return (
-    <View style={{ flex: 1, padding: 20 }}>
-      {/* Top 3 Boxes with Icons */}
+    <View style={{ flex: 1, padding: 20, backgroundColor: '#ffffff' }}>
+      {/* Four Square Boxes with Icons */}
       <View style={{ flexDirection: 'row', marginBottom: 20 }}>
-        <View
+        <TouchableOpacity
           style={{
             ...squareBoxStyles,
             marginRight: 10,
             backgroundColor: isDarkMode ? '#444' : '#f5f5f5',
           }}
+          onPress={() => {
+            /* Handle button press here */
+          }}
         >
           <Icon name="user" size={30} color={isDarkMode ? 'white' : 'black'} />
-          <Text style={{ color: isDarkMode ? 'white' : 'black' }}>页面闪退</Text>
-        </View>
-        <View
+          <Text style={{ color: isDarkMode ? 'white' : 'black' }}>账户问题</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
           style={{
             ...squareBoxStyles,
             marginRight: 10,
             backgroundColor: isDarkMode ? '#444' : '#f5f5f5',
+          }}
+          onPress={() => {
+            /* Handle button press here */
           }}
         >
           <Icon name="user" size={30} color={isDarkMode ? 'white' : 'black'} />
           <Text style={{ color: isDarkMode ? 'white' : 'black' }}>交易问题</Text>
-        </View>
-        <View
+        </TouchableOpacity>
+        <TouchableOpacity
           style={{
             ...squareBoxStyles,
             marginRight: 10,
             backgroundColor: isDarkMode ? '#444' : '#f5f5f5',
+          }}
+          onPress={() => {
+            /* Handle button press here */
           }}
         >
           <Icon name="user" size={30} color={isDarkMode ? 'white' : 'black'} />
           <Text style={{ color: isDarkMode ? 'white' : 'black' }}>操作体验</Text>
-        </View>
+        </TouchableOpacity>
       </View>
 
       {/* Two Square Boxes with Icons */}
       <View style={{ flexDirection: 'row', marginBottom: 20 }}>
-        <View
+        <TouchableOpacity
           style={{
             ...squareBoxStyles,
             marginRight: 10,
             backgroundColor: isDarkMode ? '#444' : '#f5f5f5',
           }}
+          onPress={() => {
+            /* Handle button press here */
+          }}
         >
           <Icon name="star" size={30} color={isDarkMode ? 'white' : 'black'} />
           <Text style={{ color: isDarkMode ? 'white' : 'black' }}>功能建议</Text>
-        </View>
-        <View
+        </TouchableOpacity>
+        <TouchableOpacity
           style={{
             ...squareBoxStyles,
             marginLeft: 10,
             backgroundColor: isDarkMode ? '#444' : '#f5f5f5',
           }}
+          onPress={() => {
+            /* Handle button press here */
+          }}
         >
           <Icon name="heart" size={30} color={isDarkMode ? 'white' : 'black'} />
           <Text style={{ color: isDarkMode ? 'white' : 'black' }}>其它反馈</Text>
-        </View>
+        </TouchableOpacity>
       </View>
 
       <View
@@ -84,22 +117,34 @@ const ReportQuestion = () => {
       </View>
 
       {/* Review Line */}
+
       <View
         style={{
           ...styles.reviewContainer,
           backgroundColor: isDarkMode ? '#444' : '#f5f5f5',
         }}
       >
-        <View style={styles.lineStyles}>
-          <Text
-            style={{
-              ...styles.reviewText,
-              color: isDarkMode ? 'white' : 'black',
-              marginBottom: 48,
-            }}
-          >
-            Review
-          </Text>
+        <TextInput
+          style={{
+            ...styles.reviewText,
+            color: isDarkMode ? 'white' : 'black',
+            marginBottom: 48,
+          }}
+          value={reviewText}
+          onChangeText={setReviewText}
+          placeholder="Write your review here"
+          placeholderTextColor={isDarkMode ? 'gray' : '#a9a9a9'}
+          multiline
+        />
+        <TouchableOpacity
+          style={{
+            backgroundColor: isDarkMode ? '#444' : '#f5f5f5',
+            marginLeft: 10,
+          }}
+          onPress={() => {
+            /* Handle button press here */
+          }}
+        >
           <View style={styles.plusBoxStyles}>
             <Text
               style={{
@@ -110,8 +155,10 @@ const ReportQuestion = () => {
               +
             </Text>
           </View>
-        </View>
+        </TouchableOpacity>
       </View>
+
+      {/* </View> */}
 
       <View
         style={{
@@ -143,7 +190,12 @@ const ReportQuestion = () => {
       </View>
 
       {/* Content Text Input */}
-      <View style={contentInputStyles}>
+      <TouchableOpacity
+        style={contentInputStyles}
+        onPress={() => {
+          /* Handle button press here */
+        }}
+      >
         <TextInput
           placeholder="请留下任意一个联系方式"
           placeholderTextColor={isDarkMode ? '#aaa' : '#555'}
@@ -152,15 +204,10 @@ const ReportQuestion = () => {
             color: isDarkMode ? 'white' : 'black',
           }}
         />
-      </View>
+      </TouchableOpacity>
 
       <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          style={styles.buttonStyle}
-          onPress={() => {
-            /* Handle button press here */
-          }}
-        >
+        <TouchableOpacity style={styles.buttonStyle} onPress={handlePress}>
           <Text style={styles.buttonText}>提交</Text>
         </TouchableOpacity>
       </View>
