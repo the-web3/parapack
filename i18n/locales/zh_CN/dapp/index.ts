d518bf0ab79cfb7b3544667384f62b17dbe09f21
pop <906792895@qq.com>
2024-02-23 12:50:58 +0800

    fix: report

--- i18n/locales/zh_CN/dapp/index.ts
+++ i18n/locales/zh_CN/dapp/index.ts
@@ -20,5 +20,15 @@ export default {
   "settings": "设置",
   "announcementDetails": "公告详情",
   "language": "语言",
+  "page crashes": "页面闪退",
+  "trading issues": "交易问题",
+  "operation experience": "操作体验",
+  "feature suggestions": "功能建议",
+  "other feedback": "其他反馈",
+  "I want feedback": "我要反馈",
+  "Contact Information": "联系方式",
+  "email": "邮箱",
+  "Please leave any contact information": "请留下任意一个联系方式",
+  "Submit": "提交",
   "aboutUs": "关于我们"
 };
--- screen/DApp/ReportQuestion/index.tsx
+++ screen/DApp/ReportQuestion/index.tsx
@@ -17,36 +17,37 @@ interface DAppProps {
   navigation?: any;
   mode?: string;
 }
-const typeList = [
-  {
-    icon: 'a-21',
-    text: '页面闪退',
-    type: 'broken',
-  },
-  {
-    icon: 'a-22',
-    text: '交易问题',
-    type: 'trade',
-  },
-  {
-    icon: 'a-23',
-    text: '操作体验',
-    type: 'operate',
-  },
-  {
-    icon: 'a-24',
-    text: '功能建议',
-    type: 'suggestion',
-  },
-  {
-    icon: 'a-25',
-    text: '其他反馈',
-    type: 'other',
-  },
-];
+
 const ReportQuestion = (props: DAppProps) => {
   const [imgs, setImgs] = useState<Array<string>>([]);
   const { t } = useTranslation();
+  const typeList = [
+    {
+      icon: 'a-21',
+      text: t('page crashes'),
+      type: 'broken',
+    },
+    {
+      icon: 'a-22',
+      text: t('trading issues'),
+      type: 'trade',
+    },
+    {
+      icon: 'a-23',
+      text: t('operation experience'),
+      type: 'operate',
+    },
+    {
+      icon: 'a-24',
+      text: t('feature suggestions'),
+      type: 'suggestion',
+    },
+    {
+      icon: 'a-25',
+      text: t('other feedback'),
+      type: 'other',
+    },
+  ];
   const handleImagePicker = () => {
     const options: ImageLibraryOptions = {
       mediaType: 'photo',
@@ -153,7 +154,7 @@ const ReportQuestion = (props: DAppProps) => {
                 marginTop: 30,
               }}
             >
-              我要反馈
+              {t('I want feedback')}
             </Text>
           </View>
 
@@ -221,7 +222,7 @@ const ReportQuestion = (props: DAppProps) => {
                   color: 'black',
                 }}
               >
-                联系方式
+                {t('Contact Information')}
               </Text>
               <Text
                 style={{
@@ -230,7 +231,7 @@ const ReportQuestion = (props: DAppProps) => {
                   marginLeft: 10,
                 }}
               >
-                注：邮箱
+                {t('email')}
               </Text>
             </View>
           </View>
@@ -245,7 +246,7 @@ const ReportQuestion = (props: DAppProps) => {
             }}
           >
             <TextInput
-              placeholder="请留下任意一个联系方式"
+              placeholder={`${t('Please leave any contact information')}`}
               placeholderTextColor={'#555'}
               multiline
               onChangeText={setContact}
@@ -256,7 +257,7 @@ const ReportQuestion = (props: DAppProps) => {
               }}
             />
           </View>
-          <Button onPress={handlePress}>提交</Button>
+          <Button onPress={handlePress}>{t('Submit')}</Button>
         </View>
       </SafeAreaView>
     </Layout>
